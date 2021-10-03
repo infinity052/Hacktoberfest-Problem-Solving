9694f4e4bf05b105cafe6e9460cd728014982e52
infinity0521998 <91830290+infinity0521998@users.noreply.github.com>
2021-10-03 17:27:38 +0530

    isPalindrome
    https://leetcode.com/problems/palindrome-number/

--- index.cpp
+++ index.cpp
@@ -261,6 +261,28 @@ int lengthOfLongestSubstring(string s) {
         return ans;
     }
 
+bool isPalindrome(int x) {
+        int a;
+        long sum=0;
+        int b;
+        b=x;
+        while(b>0)
+        {
+            a=b%10;
+            sum=sum*10+a;
+            b=b/10;
+        }
+        if(sum==x)
+        {
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
+        
+    }
+
 int main(){
   return 0;
 }
